297949a14abe3a7f2e67fac91b68e8070e043540
Unknown <michal.kulesz@gmail.com>
2017-01-14 09:47:39 +0100

    added name and so on.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,12 +9,45 @@ This is empty on purpose! Your code to build the resume will go here.
 
  //$("#main").append(funthoughts);
 
- var name = "Michał Kulesz";
- var role = "Konsultant SAP";
 
- var formattedName = HTMLheaderName.replace("%data%", name);
- var formattedRole = HTMLheaderRole.replace("%data%", role);
-
- 
+ /*
  $("#header").prepend(formattedRole);
  $("#header").prepend(formattedName);
+*/
+var skills = ["ABAP", "programming", "Java"];
+var contactInfo = "tel. 123456789";
+var pictureURL = "images/fry.jpg";
+var welcomeMessage = "Witam";
+
+var bio = {
+	"name" : "Michał Kulesz",
+	"role" : "Konsultant SAP",
+	"contacts" : {
+		"mobile" : contactInfo
+	} ,
+	"welcomeMessage" : welcomeMessage,
+	"skills" : skills ,
+	"biopic" : pictureURL
+};
+
+var work = { };
+	work.jobPosition = "Młodszy programista ABAP";
+	work.employer = "KBJ";
+	work.yearsWorked = "2 months";
+	work.city = "Warszawa";
+
+var education = { };
+	education["name"] = "Uniwersytet Gdański";
+	education["years"] = "5 years";
+	education["city"] = "Gdańsk";
+
+ var formattedName = HTMLheaderName.replace("%data%", bio.name);
+ var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+
+ 
+
+ $("#main").append(education.name);
+ $("#main").append(work["jobPosition"]);
